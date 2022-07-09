9398b9615f5058c4d8608d829f01416a3300c078
pmengy <936841379@qq.com>
2022-07-09 20:58:15 +0800

    完成hash工作原理

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <div></div>
+  <div>
+    <h1>App组件</h1>
+    <a href="#/myInfo">信息</a>&nbsp; <a href="#/myOrders">订单</a>&nbsp;
+    <a href="#/index">首页</a>&nbsp;
+    <component :is="comName"></component>
+  </div>
 </template>
 
 <script>
+import MyInfo from '@/views/MyInfo.vue';
+import MyOrders from '@/views/MyOrders.vue';
+import _Index from '@/views/_Index.vue';
 export default {
   data() {
-    return {};
+    return {
+      comName: '_Index',
+    };
   },
-
-  mounted() {},
-
+  created() {
+    // 改变浏览器url的hash值, JS监听到hash值改变, 把对应的组件显示到同一个挂载点
+    window.onhashchange = () => {
+      switch (location.hash) {
+        case '#/myInfo':
+          this.comName = 'MyInfo';
+          break;
+        case '#/myOrders':
+          this.comName = 'MyOrders';
+          break;
+        case '#/index':
+          this.comName = '_Index';
+          break;
+      }
+    };
+  },
   methods: {},
+  components: {
+    MyInfo,
+    MyOrders,
+    _Index,
+  },
 };
 </script>
